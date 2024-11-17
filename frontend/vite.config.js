4e56aa952607fe54ae737cc723a17ef8cad24480
koushikxahmed <koushikxahmed@gmail.com>
2024-11-17 15:22:01 +0600

    vite file updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          "https://job-tracker-au74c2qu3-koushik-ahmeds-projects.vercel.app/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
